5c5ec18a1123f9570fe941aacd71d25b3c6b8bcf
antoniobasileo <antonio.basileo92@gmail.com>
2026-02-14 14:20:49 +0100

    Minor fix

--- src/main/java/it/auth/security/starter/AuthSecurityStarterApplication.java
+++ src/main/java/it/auth/security/starter/AuthSecurityStarterApplication.java
@@ -1,13 +1,12 @@
 package it.auth.security.starter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@AutoConfiguration
+@ComponentScan(basePackages = {"it.auth.security.starter"})
 public class AuthSecurityStarterApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(AuthSecurityStarterApplication.class, args);
-    }
 
 }
