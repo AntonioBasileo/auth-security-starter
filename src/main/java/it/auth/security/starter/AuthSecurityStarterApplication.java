39abc4f833460886183bdef682f84aba6148d8b7
antoniobasileo <antonio.basileo92@gmail.com>
2026-02-15 21:18:52 +0100

    minor fix

--- src/main/java/it/auth/security/starter/AuthSecurityStarterApplication.java
+++ src/main/java/it/auth/security/starter/AuthSecurityStarterApplication.java
@@ -2,9 +2,11 @@ package it.auth.security.starter;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @AutoConfiguration
 @ComponentScan(basePackages = {"it.auth.security.starter"})
+@EnableJpaRepositories(basePackages = {"it.auth.security.starter.repository"})
 public class AuthSecurityStarterApplication {
 
 }
